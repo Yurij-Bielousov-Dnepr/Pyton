bda7a0b6ed47d69f710b450370713f98c9362a01
NikolayKalyshnuy <nikkalaba245@gmail.com>
2022-11-30 20:12:23 +0200

    replace metrics

--- byv_hq_10.py
+++ byv_hq_10.py
@@ -1,14 +1,14 @@
 metrics=[]
 def bubblesort(sorting_list: list,metrics):
-    n=p=0
+    # n=p=0
     list_сopy=sorting_list.copy()
     for i in range(len(list_сopy)):
         for j in range(len(list_сopy)-1-i):
-            n += 1
+            metrics[0] += 1
             if list_сopy[j] > list_сopy[j+1]:
                 list_сopy[j], list_сopy[j+1] = list_сopy[j+1], list_сopy[j]
-                p+= 1
-    metrics.append([n,p])
+                metrics[1]+= 1
+    # metrics.append([n,p])
     return list_сopy
 def SortInsert(sorting_list, metrics, start = 1):
     n=p=0
@@ -43,40 +43,36 @@ def SortShell(sorting_list, metrics):
     return sorting_list_tmp
 
 def SortMerge(sorting_list, metrics):
-    p=0
-    n=0
+    # p=0
+    # n=0
     sorting_list_tmp=sorting_list.copy()
     if len(sorting_list_tmp) > 1:
+        metrics[0]+=1
         nn = len(sorting_list_tmp) // 2
         leftPart = sorting_list_tmp[:nn]
         rightPart = sorting_list_tmp[nn:]
-        n+=1
         SortMerge(leftPart, metrics)
-        n+=1
         SortMerge(rightPart, metrics)
         i = j = k = 0
         while i < len(leftPart) and j < len(rightPart):
             if leftPart[i] < rightPart[j]:
                 sorting_list_tmp[k] = leftPart[i]
-                p+=1
                 i+=1
             else:
                 sorting_list_tmp[k] = rightPart[j]
                 j+=1
-                p+=1
+            metrics[1]+=1
             k+=1
         while i < len(leftPart):
             sorting_list_tmp[k] = leftPart[i]
             i+=1
-            p+=1
+            metrics[1]+=1
             k+=1
         while j < len(rightPart):
             sorting_list_tmp[k] = rightPart[j]
             j+=1
-            p+=1
+            metrics[1]+=1
             k+=1
-    metrics.append([n,p])
-
     return sorting_list_tmp
 
 # К пройденным методам добавить метрики кол-ва перестановок и циклов
@@ -88,62 +84,74 @@ def SortMerge(sorting_list, metrics):
 list_best = [-2,0,1,2,3,4,5]
 list_bad = [5,4,3,2,1,0,-2]
 list_normal =[-2,0,4,2,3,5,1] 
-metrics=([],[])
+metrics=[]
+metrics.append([0,0])
 print("Сортировка Пузырьками, лучший случай")
 print("Исходные данные       ",list_best)
 print("Отсортированные данные",bubblesort(list_best,metrics[0]))
 print("При сортировке сделано ",metrics[0][0],"проходов и замена сделана",metrics[0][1],"раз")
 
+metrics.append([0,0])
 print("Сортировка Пузырьками, худший случай")
 print("Исходные данные       ",list_bad)
 print("Отсортированные данные",bubblesort(list_bad,metrics[1]))
 print("При сортировке сделано ",metrics[1][0],"проходов и замена сделана",metrics[1][1],"раз")
 
+metrics.append([0,0])
 print("Сортировка Пузырьками, средний случай")
 print("Исходные данные       ",list_normal)
 print("Отсортированные данные",bubblesort(list_normal, metrics[2]))
 print("При сортировке сделано ",metrics[2][0],"проходов и замена сделана",metrics[2][1],"раз")
 
+metrics.append([0,0])
 print("Сортировка вставками, лучший случай")
 print("Исходные данные       ",list_best)
 print("Отсортированные данные",SortInsert(list_best, metrics[3]))
 print("При сортировке сделано ",metrics[3][0],"проходов и замена сделана",metrics[3][1],"раз")
 
+metrics.append([0,0])
 print("Сортировка вставками, худший случай")
 print("Исходные данные       ",list_bad)
 print("Отсортированные данные",SortInsert(list_bad, metrics[4]))
 print("При сортировке сделано ",metrics[4][0],"проходов и замена сделана",metrics[4][1],"раз")
 
+metrics.append([0,0])
 print("Сортировка вставками, средний случай")
 print("Исходные данные       ",list_normal)
 print("Отсортированные данные",SortInsert(list_normal, metrics[5]))
 print("При сортировке сделано ",metrics[5][0],"проходов и замена сделана",metrics[5][1],"раз")
 
+metrics.append([0,0])
 print("Сортировка методом Шелла, лучший случай")
 print("Исходные данные       ",list_best)
 print("Отсортированные данные",SortShell(list_best, metrics[6]))
 print("При сортировке сделано ",metrics[6][0],"проходов и замена сделана",metrics[6][1],"раз")
 
+metrics.append([0,0])
 print("Сортировка методом Шелла, худший случай")
 print("Исходные данные       ",list_bad)
 print("Отсортированные данные",SortShell(list_bad, metrics[7]))
 print("При сортировке сделано ",metrics[7][0],"проходов и замена сделана",metrics[7][1],"раз")
 
+metrics.append([0,0])
 print("Сортировка методом Шелла, средний случай")
 print("Исходные данные       ",list_normal)
 print("Отсортированные данные",SortShell(list_normal, metrics[8]))
 print("При сортировке сделано ",metrics[8][0],"проходов и замена сделана",metrics[8][1],"раз")
 
+metrics.append([0,0])
 print("Сортировка объединением, лучший случай")
 print("Исходные данные       ",list_best)
 print("Отсортированные данные",SortMerge(list_best, metrics[9]))
 print("При сортировке сделано ",metrics[9][0],"проходов и замена сделана",metrics[9][1],"раз")
 
+metrics.append([0,0])
 print("Сортировка объединением, худший случай")
 print("Исходные данные       ",list_bad)
 print("Отсортированные данные",SortMerge(list_bad, metrics[10]))
 print("При сортировке сделано ",metrics[10][0],"проходов и замена сделана",metrics[10][1],"раз")
 
+metrics.append([0,0])
 print("Сортировка объединением, средний случай")
 print("Исходные данные       ",list_normal)
 print("Отсортированные данные",SortMerge(list_normal, metrics[11]))
